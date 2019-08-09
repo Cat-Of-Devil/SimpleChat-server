fdbfd36674e1acd994e2b3a8e84a1f7367ccc14d
wxmaper <wxmaper@gmail.com>
2019-08-10 00:32:03 +0700

    Update ChatWorker.php

--- ChatWorker.php
+++ ChatWorker.php
@@ -11,6 +11,39 @@ $connections = []; // сюда будем складывать все подкл
 // Стартуем WebSocket-сервер на порту 27800
 $worker = new Worker("websocket://0.0.0.0:27800");
 
+$worker->onWorkerStart = function($worker) use (&$connections)
+{
+    $interval = 5; // пингуем каждые 5 секунд
+    Timer::add($interval, function() use(&$connections) {
+        foreach ($connections as $c) {
+            // Если ответ не пришел 3 раза, то удаляем соединение из списка
+            // и оповещаем всех участников об "отвалившемся" пользователе
+            if ($c->pingWithoutResponseCount >= 3) {
+                unset($connections[$c->id]);
+                
+                $messageData = [
+                    'action' => 'ConnectionLost',
+                    'userId' => $c->id,
+                    'userName' => $c->userName,
+                    'gender' => $c->gender,
+                    'userColor' => $c->userColor
+                ];
+                $message = json_encode($messageData);
+                
+                $c->destroy(); // уничтожаем соединение
+                
+                foreach ($connections as $c) {
+                    $c->send($message);
+                }
+            }
+            else {
+                $c->send('{"action":"Ping"}');
+                $c->pingWithoutResponseCount++; // увеличиваем счетчик пингов
+            }
+        }
+    });
+};
+
 $worker->onConnect = function($connection) use(&$connections)
 {
     // Эта функция выполняется при подключении пользователя к WebSocket-серверу
@@ -107,6 +140,76 @@ $worker->onConnect = function($connection) use(&$connections)
             $c->send($message);
         }
     };
+};
+
+$worker->onClose = function($connection) use(&$connections)
+{
+    // Эта функция выполняется при закрытии соединения
+    if (!isset($connections[$connection->id])) {
+        return;
+    }
+    
+    // Удаляем соединение из списка
+    unset($connections[$connection->id]);
+    
+    // Оповещаем всех пользователей о выходе участника из чата
+    $messageData = [
+        'action' => 'Disconnected',
+        'userId' => $connection->id,
+        'userName' => $connection->userName,
+        'gender' => $connection->gender,
+        'userColor' => $connection->userColor
+    ];
+    $message = json_encode($messageData);
+    
+    foreach ($connections as $c) {
+        $c->send($message);
+    }
+};
+
+$worker->onMessage = function($connection, $message) use (&$connections)
+{
+    $messageData = json_decode($message, true);
+    $toUserId = isset($messageData['toUserId']) ? (int) $messageData['toUserId'] : 0;
+    $action = isset($messageData['action']) ? $messageData['action'] : '';
+    
+    if ($action == 'Pong') {
+        // При получении сообщения "Pong", обнуляем счетчик пингов
+        $connection->pingWithoutResponseCount = 0;
+    }
+    else {
+        // Дополняем сообщение данными об отправителе
+        $messageData['userId'] = $connection->id;
+        $messageData['userName'] = $connection->userName;
+        $messageData['gender'] = $connection->gender;
+        $messageData['userColor'] = $connection->userColor;
+        
+        // Преобразуем специальные символы в HTML-сущности в тексте сообщения
+        $messageData['text'] = htmlspecialchars($messageData['text']);
+        // Заменяем текст заключенный в фигурные скобки на жирный
+        $messageData['text'] = preg_replace('/\{(.*)\}/u', '<b>\\1</b>', $messageData['text']);
+        
+        if ($toUserId == 0) {
+            // Отправляем сообщение всем пользователям
+            $messageData['action'] = 'PublicMessage';
+            foreach ($connections as $c) {
+                $c->send(json_encode($messageData));
+            }
+        }
+        else {
+            $messageData['action'] = 'PrivateMessage';
+            if (isset($connections[$toUserId])) {
+                // Отправляем приватное сообщение указанному пользователю
+                $connections[$toUserId]->send(json_encode($messageData));
+                // и отправителю
+                $connections->send(json_encode($messageData));
+            }
+            else {
+                $messageData['text'] = 'Не удалось отправить сообщение выбранному пользователю';
+                $connection->send(json_encode($messageData));
+            }
+        }
+    }
 };
 
 Worker::runAll();
